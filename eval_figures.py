a7d4bdb7eab742069a6f4ceccb02f4e1b575a442
Zach Nussbaum <zanussbaum@gmail.com>
2023-04-08 04:45:52 +0000

    feat: eval faster

--- eval_figures.py
+++ eval_figures.py
@@ -5,8 +5,8 @@ from matplotlib import pyplot as plt
 
 plt.figure()
 for fpath in glob.glob('./eval_data/*.pkl'):
-    parts = fpath.split('__')
-    model_name = parts[1].replace('model-', '').replace('.pkl', '')
+    parts = fpath.split('_')
+    model_name = "-".join(fpath.replace(".pkl", "").split("_")[2:])
     lora_name = parts[2].replace('lora-', '').replace('.pkl', '')
     with open(fpath, 'rb') as f:
         data = pickle.load(f)
@@ -14,7 +14,7 @@ for fpath in glob.glob('./eval_data/*.pkl'):
         perplexities = np.nan_to_num(perplexities, 100)
         perplexities = np.clip(perplexities, 0, 100)
         if 'nomic' in fpath:
-            label = 'GPT4all-lora'
+            label = f'GPT4all-{model_name}'
         else:
             label = 'alpaca-lora'
         plt.hist(perplexities, label=label, alpha=.5)
