305975451c8b3cb2d108bc6da6211f39b7078d2a
Adam Treat <treat.adam@gmail.com>
2023-04-13 22:15:40 -0400

    Add an abstraction around gpt-j that will allow other arch models to be loaded in ui.

---
+++ llmodel.h
@@ -0,0 +1,24 @@
+#ifndef LLMODEL_H
+#define LLMODEL_H
+
+#include <string>
+#include <functional>
+#include <vector>
+
+class LLModel {
+public:
+    explicit LLModel() {}
+    virtual ~LLModel() {}
+
+    virtual bool loadModel(const std::string &modelPath, std::istream &fin) = 0;
+    virtual bool isModelLoaded() const = 0;
+    struct PromptContext {
+        std::vector<float> logits;
+        int32_t n_past = 0; // number of tokens in past conversation
+    };
+    virtual void prompt(const std::string &prompt, std::function<bool(const std::string&)> response,
+        PromptContext &ctx, int32_t n_predict = 200, int32_t top_k = 40, float top_p = 0.9f,
+        float temp = 0.9f, int32_t n_batch = 9) = 0;
+};
+
+#endif // LLMODEL_H
