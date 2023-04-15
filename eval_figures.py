d0ee1449354b887a4a947ff130017d56d79b134d
yoni <yoni.kremer@gmail.com>
2023-04-15 19:09:03 +0300

    deleted unused variable

--- eval_figures.py
+++ eval_figures.py
@@ -6,14 +6,13 @@ from matplotlib import pyplot as plt
 plt.figure()
 for fpath in glob.glob('./eval_data/*.pkl'):
     parts = fpath.split('__')
-    model_name = "-".join(fpath.replace(".pkl", "").split("_")[2:])
     with open(fpath, 'rb') as f:
         data = pickle.load(f)
         perplexities = data['perplexities']
         perplexities = np.nan_to_num(perplexities, 100)
         perplexities = np.clip(perplexities, 0, 100)
         if 'alpaca' not in fpath:
-            identifier = model_name = "-".join(fpath.replace(".pkl", "").split("eval__model-")[1:]) 
+            identifier = "-".join(fpath.replace(".pkl", "").split("eval__model-")[1:])
             label = 'GPT4all-'
             label += identifier
             
@@ -25,4 +24,3 @@ plt.xlabel('Perplexity')
 plt.ylabel('Frequency')
 plt.legend()
 plt.savefig('figs/perplexity_hist.png')
-
