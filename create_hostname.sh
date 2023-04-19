97a1cd05395e4ab36f197384774bbf79d26a4971
zanussbaum <zanussbaum@gmail.com>
2023-04-19 18:13:24 -0400

    chore: delete unused

--- create_hostname.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export WORKER_IP=$1
-N_GPUS=8
-# create dir if doesn't exist
-sudo mkdir -p /job
-printf "localhost slots=$N_GPUS\n$WORKER_IP slots=$N_GPUS" | sudo tee /job/hostfile
-echo /job/hostfile
