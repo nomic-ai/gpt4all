35e7503571d6405a71aaf82982c959bd6cb936e3
Adam Treat <treat.adam@gmail.com>
2023-04-23 19:43:20 -0400

    Make the download use a temp file to save ram and make it threaded.

--- download.h
+++ download.h
@@ -6,12 +6,15 @@
 #include <QNetworkReply>
 #include <QFile>
 #include <QVariant>
+#include <QTemporaryFile>
+#include <QThread>
 
 struct ModelInfo {
     Q_GADGET
     Q_PROPERTY(QString filename MEMBER filename)
     Q_PROPERTY(QString filesize MEMBER filesize)
     Q_PROPERTY(QByteArray md5sum MEMBER md5sum)
+    Q_PROPERTY(bool calcHash MEMBER calcHash)
     Q_PROPERTY(bool installed MEMBER installed)
     Q_PROPERTY(bool isDefault MEMBER isDefault)
 
@@ -19,10 +22,29 @@ public:
     QString filename;
     QString filesize;
     QByteArray md5sum;
+    bool calcHash = false;
     bool installed = false;
     bool isDefault = false;
 };
 Q_DECLARE_METATYPE(ModelInfo)
+
+class HashAndSaveFile : public QObject
+{
+    Q_OBJECT
+public:
+    HashAndSaveFile();
+
+public Q_SLOTS:
+    void hashAndSave(const QString &hash, const QString &saveFilePath,
+        QTemporaryFile *tempFile, QNetworkReply *modelReply);
+
+Q_SIGNALS:
+    void hashAndSaveFinished(bool success,
+        QTemporaryFile *tempFile, QNetworkReply *modelReply);
+
+private:
+    QThread m_hashAndSaveThread;
+};
 
 class Download : public QObject
 {
@@ -42,18 +64,24 @@ private Q_SLOTS:
     void handleJsonDownloadFinished();
     void handleDownloadProgress(qint64 bytesReceived, qint64 bytesTotal);
     void handleModelDownloadFinished();
+    void handleHashAndSaveFinished(bool success,
+        QTemporaryFile *tempFile, QNetworkReply *modelReply);
+    void handleReadyRead();
 
 Q_SIGNALS:
     void downloadProgress(qint64 bytesReceived, qint64 bytesTotal, const QString &modelFile);
     void downloadFinished(const QString &modelFile);
     void modelListChanged();
+    void requestHashAndSave(const QString &hash, const QString &saveFilePath,
+        QTemporaryFile *tempFile, QNetworkReply *modelReply);
 
 private:
     void parseJsonFile(const QByteArray &jsonData);
 
+    HashAndSaveFile *m_hashAndSave;
     QMap<QString, ModelInfo> m_modelMap;
     QNetworkAccessManager m_networkManager;
-    QVector<QNetworkReply*> m_activeDownloads;
+    QMap<QNetworkReply*, QTemporaryFile*> m_activeDownloads;
 
 private:
     explicit Download();
