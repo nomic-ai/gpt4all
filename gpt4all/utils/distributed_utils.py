586a8abc0673a71f1d22d5399515406d6da83b46
Zach Nussbaum <zanussbaum@gmail.com>
2023-04-25 20:34:12 +0000

    fix: allow for print for fns that are used in both dist and single

--- gpt4all/utils/distributed_utils.py
+++ gpt4all/utils/distributed_utils.py
@@ -2,5 +2,8 @@ import torch.distributed as dist
 
 
 def rank0_print(msg):
-    if dist.get_rank() == 0:
+    if dist.is_initialized():
+        if dist.get_rank() == 0:
+            print(msg)
+    else:
         print(msg)
