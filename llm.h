679b61ee07d726695893aefd9c167ead1a7ecefd
Adam Treat <treat.adam@gmail.com>
2023-05-01 14:24:16 -0400

    Provide convenience methods for adding/removing/changing chat.

--- llm.h
+++ llm.h
@@ -9,26 +9,39 @@ class LLM : public QObject
 {
     Q_OBJECT
     Q_PROPERTY(QList<QString> modelList READ modelList NOTIFY modelListChanged)
-    Q_PROPERTY(Chat *currentChat READ currentChat NOTIFY currentChatChanged)
     Q_PROPERTY(bool isRecalc READ isRecalc NOTIFY recalcChanged)
+    Q_PROPERTY(Chat *currentChat READ currentChat NOTIFY currentChatChanged)
+    Q_PROPERTY(QList<QString> chatList READ chatList NOTIFY chatListChanged)
 
 public:
 
     static LLM *globalInstance();
 
     QList<QString> modelList() const;
-    Q_INVOKABLE bool checkForUpdates() const;
-    Chat *currentChat() const { return m_currentChat; }
     bool isRecalc() const;
+    Chat *currentChat() const;
+    QList<QString> chatList() const;
+
+    Q_INVOKABLE QString addChat();
+    Q_INVOKABLE void removeChat(const QString &id);
+    Q_INVOKABLE Chat *chatFromId(const QString &id) const;
+    Q_INVOKABLE void setCurrentChatFromId(const QString &id);
+    Q_INVOKABLE bool checkForUpdates() const;
 
 Q_SIGNALS:
     void modelListChanged();
     void currentChatChanged();
     void recalcChanged();
+    void chatListChanged();
     void responseChanged();
 
 private:
-    Chat *m_currentChat;
+    void connectChat(Chat *chat);
+    void disconnectChat(Chat *chat);
+
+private:
+    QString m_currentChat;
+    QMap<QString, Chat*> m_chats;
 
 private:
     explicit LLM();
