d3ec3333140d579227c618f511bb627a53d100fa
Adam Treat <treat.adam@gmail.com>
2023-05-08 18:31:20 -0400

    Allow these to load for gptj too.

--- llmodel/gptj.cpp
+++ llmodel/gptj.cpp
@@ -210,6 +210,7 @@ bool gptj_model_load(const std::string &fname, std::istream &fin, gptj_model & m
         case 1: wtype = GGML_TYPE_F16;  break;
         case 2: wtype = GGML_TYPE_Q4_0; break;
         case 3: wtype = GGML_TYPE_Q4_1; break;
+        case 5: wtype = GGML_TYPE_Q4_2; break;
         default:
                 {
                     fprintf(stderr, "%s: invalid model file '%s' (bad f16 value %d)\n",
