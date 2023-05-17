58003857380e33c1e8c6236d9de1022b65d1461f
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2023-05-17 01:18:03 -0500

    progress on prompting and some helper methods

--- gpt4all-bindings/typescript/src/gpt4all.d.ts
+++ gpt4all-bindings/typescript/src/gpt4all.d.ts
@@ -3,6 +3,19 @@ declare module 'gpt4all-ts';
 
 //todo
 declare class LLModel {
+
+    constructor(path: string);
+    /**
+     * Get the size of the internal state of the model.
+     * NOTE: This state data is specific to the type of model you have created.
+     * @param model A pointer to the llmodel_model instance.
+     * @return the size in bytes of the internal state of the model
+     */
+    stateSize(): number;
+
+    threadCount() : number;
+
+    setThreadCount(newNumber: number): void
 }
 
 export { LLModel }
