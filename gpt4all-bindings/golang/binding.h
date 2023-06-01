2a6b6fc1b59cb4a4c87214e85d5372fe7bac6593
mudler <mudler@mocaccino.org>
2023-06-01 14:37:14 +0200

    Adapt code

--- gpt4all-bindings/golang/binding.h
+++ gpt4all-bindings/golang/binding.h
@@ -4,16 +4,12 @@ extern "C" {
 
 #include <stdbool.h>
 
-void* load_mpt_model(const char *fname, int n_threads);
+void* load_gpt4all_model(const char *fname, int n_threads);
 
-void* load_llama_model(const char *fname, int n_threads);
-
-void* load_gptj_model(const char *fname, int n_threads);
-
-void gptj_model_prompt( const char *prompt, void *m, char* result, int repeat_last_n, float repeat_penalty, int n_ctx, int tokens, int top_k,
+void gpt4all_model_prompt( const char *prompt, void *m, char* result, int repeat_last_n, float repeat_penalty, int n_ctx, int tokens, int top_k,
                             float top_p, float temp, int n_batch,float ctx_erase);
 
-void gptj_free_model(void *state_ptr);
+void gpt4all_free_model(void *state_ptr);
 
 extern unsigned char getTokenCallback(void *, char *);
 
