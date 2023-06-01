8e89ceb54bf6bded5ac48a9e7405a5e2b31921f9
mvenditto <venditto.matteo@gmail.com>
2023-06-01 22:01:27 +0200

    C# Bindings - improved logging (#714)
    * added optional support for .NET logging
    
    * bump version and add missing alpha suffix
    
    * avoid creating additional namespace for extensions
    
    * prefer NullLogger/NullLoggerFactory over null-conditional ILogger to avoid errors
    
    ---------
    
    Signed-off-by: mvenditto <venditto.matteo@gmail.com>

---
+++ gpt4all-bindings/csharp/Gpt4All/Extensions/LLPromptContextExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Gpt4All.Bindings;
+
+namespace Gpt4All;
+
+internal static class LLPromptContextExtensions
+{
+    public static string Dump(this LLModelPromptContext context)
+    {
+        var ctx = context.UnderlyingContext;
+        return @$"
+        {{
+            logits_size = {ctx.logits_size}
+            tokens_size = {ctx.tokens_size}
+            n_past = {ctx.n_past}
+            n_ctx = {ctx.n_ctx}
+            n_predict = {ctx.n_predict}
+            top_k = {ctx.top_k}
+            top_p = {ctx.top_p}
+            temp = {ctx.temp}
+            n_batch = {ctx.n_batch}
+            repeat_penalty = {ctx.repeat_penalty}
+            repeat_last_n = {ctx.repeat_last_n}
+            context_erase = {ctx.context_erase}
+        }}";
+    }
+}
