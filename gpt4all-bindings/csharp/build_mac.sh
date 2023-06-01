ed68916a41814709be11f65d445a6ef45b6afb89
Tim Miller <innerlogic4321@gmail.com>
2023-06-01 22:32:19 +0900

    Update build scripts for mac/linux

--- gpt4all-bindings/csharp/build_mac.sh
+++ gpt4all-bindings/csharp/build_mac.sh
@@ -5,4 +5,6 @@ mkdir runtimes/osx/build
 cmake -S ../../gpt4all-backend -B runtimes/osx/build -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64"
 cmake --build runtimes/osx/build --parallel --config Release
 cp runtimes/osx/build/libllmodel.dylib  runtimes/osx/native/libllmodel.dylib
-cp runtimes/osx/build/llama.cpp/libllama.dylib runtimes/osx/native/libllama.dylib
+cp runtimes/osx/build/libgptj*.dylib  runtimes/osx/native/
+cp runtimes/osx/build/libllama*.dylib  runtimes/osx/native/
+cp runtimes/osx/build/libmpt*.dylib  runtimes/osx/native/
