fc5537badfd1d21f714bf43d3c3221535c15308d
mvenditto <venditto.matteo@gmail.com>
2023-06-01 22:01:27 +0200

    C# Bindings - improved logging (#714)
    * added optional support for .NET logging
    
    * bump version and add missing alpha suffix
    
    * avoid creating additional namespace for extensions
    
    * prefer NullLogger/NullLoggerFactory over null-conditional ILogger to avoid errors
    
    ---------
    
    Signed-off-by: mvenditto <venditto.matteo@gmail.com>

--- gpt4all-bindings/csharp/Gpt4All/Model/Gpt4AllModelFactory.cs
+++ gpt4all-bindings/csharp/Gpt4All/Model/Gpt4AllModelFactory.cs
@@ -1,13 +1,26 @@
-﻿using Gpt4All.Bindings;
-using System.Diagnostics;
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Logging;
+using Gpt4All.Bindings;
+using Microsoft.Extensions.Logging.Abstractions;
 
 namespace Gpt4All;
 
 public class Gpt4AllModelFactory : IGpt4AllModelFactory
 {
-    private static IGpt4AllModel CreateModel(string modelPath, ModelType? modelType = null)
+    private readonly ILoggerFactory _loggerFactory;
+    private readonly ILogger _logger;
+
+    public Gpt4AllModelFactory(ILoggerFactory? loggerFactory = null)
+    {
+        _loggerFactory = loggerFactory ?? NullLoggerFactory.Instance;
+        _logger = _loggerFactory.CreateLogger<Gpt4AllModelFactory>();
+    }
+
+    private IGpt4AllModel CreateModel(string modelPath, ModelType? modelType = null)
     {
         var modelType_ = modelType ?? ModelFileUtils.GetModelTypeFromModelFileHeader(modelPath);
+
+        _logger.LogInformation("Creating model path={ModelPath} type={ModelType}", modelPath, modelType_);
 
         var handle = modelType_ switch
         {
@@ -17,18 +30,25 @@ public class Gpt4AllModelFactory : IGpt4AllModelFactory
             _ => NativeMethods.llmodel_model_create(modelPath),
         };
 
-        var loadedSuccesfully = NativeMethods.llmodel_loadModel(handle, modelPath);
+        _logger.LogDebug("Model created handle=0x{ModelHandle:X8}", handle);
+        _logger.LogInformation("Model loading started");
 
-        if (loadedSuccesfully == false)
+        var loadedSuccessfully = NativeMethods.llmodel_loadModel(handle, modelPath);
+
+        _logger.LogInformation("Model loading completed success={ModelLoadSuccess}", loadedSuccessfully);
+
+        if (loadedSuccessfully == false)
         {
             throw new Exception($"Failed to load model: '{modelPath}'");
         }
 
-        var underlyingModel = LLModel.Create(handle, modelType_);
+        var logger = _loggerFactory.CreateLogger<LLModel>();
+
+        var underlyingModel = LLModel.Create(handle, modelType_, logger: logger);
 
         Debug.Assert(underlyingModel.IsLoaded());
 
-        return new Gpt4All(underlyingModel);
+        return new Gpt4All(underlyingModel, logger: logger);
     }
 
     public IGpt4AllModel LoadModel(string modelPath) => CreateModel(modelPath, modelType: null);
