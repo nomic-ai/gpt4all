70e3b7e907a887c04863aef6197e9802284a1208
Adam Treat <treat.adam@gmail.com>
2023-06-02 10:47:12 -0400

    Try and fix build on mac.

--- gpt4all-backend/llmodel.cpp
+++ gpt4all-backend/llmodel.cpp
@@ -96,28 +96,6 @@ const LLModel::Implementation* LLModel::implementation(std::ifstream& f, const s
     return nullptr;
 }
 
-void LLModel::recalculateContext(PromptContext &promptCtx, std::function<bool(bool)> recalculate) {
-    size_t i = 0;
-    promptCtx.n_past = 0;
-    while (i < promptCtx.tokens.size()) {
-        size_t batch_end = std::min(i + promptCtx.n_batch, promptCtx.tokens.size());
-        std::vector<int32_t> batch(promptCtx.tokens.begin() + i, promptCtx.tokens.begin() + batch_end);
-        assert(promptCtx.n_past + int32_t(batch.size()) <= promptCtx.n_ctx);
-        if (!evalTokens(promptCtx, batch)) {
-            std::cerr << "LLModel ERROR: Failed to process prompt\n";
-            goto stop_generating;
-        }
-        promptCtx.n_past += batch.size();
-        if (!recalculate(true))
-            goto stop_generating;
-        i = batch_end;
-    }
-    assert(promptCtx.n_past == int32_t(promptCtx.tokens.size()));
-
-stop_generating:
-    recalculate(false);
-}
-
 LLModel *LLModel::construct(const std::string &modelPath, std::string buildVariant) {
     //TODO: Auto-detect CUDA/OpenCL
     if (buildVariant == "auto") {
