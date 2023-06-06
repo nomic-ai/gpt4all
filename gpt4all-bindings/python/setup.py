a3aab14f61c435a1432539b28a4a1b9da92ac616
Andriy Mulyar <andriy.mulyar@gmail.com>
2023-06-06 16:09:16 -0400

    Supports downloading officially supported models not hosted on gpt4all R2

--- gpt4all-bindings/python/setup.py
+++ gpt4all-bindings/python/setup.py
@@ -61,7 +61,7 @@ copy_prebuilt_C_lib(SRC_CLIB_DIRECtORY,
 
 setup(
     name=package_name,
-    version="0.3.0",
+    version="0.3.1",
     description="Python bindings for GPT4All",
     author="Richard Guo",
     author_email="richard@nomic.ai",
