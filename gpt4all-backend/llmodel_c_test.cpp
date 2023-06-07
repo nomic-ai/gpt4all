6e60d52f57c6165b56e9d15b7c82ecdf333573ac
niansa <anton-sa@web.de>
2023-06-07 19:08:51 +0200

    Made tests configurable with different models via json file

--- gpt4all-backend/llmodel_c_test.cpp
+++ gpt4all-backend/llmodel_c_test.cpp
@@ -1,4 +1,5 @@
 #include "llmodel_c.h"
+#include "macros.h"
 
 #include <string>
 #include <string_view>
@@ -10,7 +11,7 @@ namespace {
 llmodel_model model;
 uint8_t *savedState;
 
-TEST(CAPITest, ModelLoad) {
+TEST(GET_SUITE_NAME(CAPITest_), ModelLoad) {
     llmodel_error ec;
     llmodel_set_implementation_search_path("..");
     model = llmodel_model_create2(LLMODEL_TEST_MODEL, "auto", &ec);
@@ -20,7 +21,7 @@ TEST(CAPITest, ModelLoad) {
     EXPECT_TRUE(llmodel_isModelLoaded(model));
 }
 
-TEST(CAPITest, Prompt) {
+TEST(GET_SUITE_NAME(CAPITest_), Prompt) {
     static std::string response;
 
     auto prompt_cb = [] (int32_t) {
@@ -28,7 +29,7 @@ TEST(CAPITest, Prompt) {
     };
     auto response_cb = [] (int32_t, const char *token) {
         response += token;
-        return true;
+        return response.size() < LLMODEL_TEST_RESPONSE_LEN;
     };
     auto recalc_cb = [] (bool is_recalculating) {
         EXPECT_FALSE(is_recalculating);
@@ -42,27 +43,28 @@ TEST(CAPITest, Prompt) {
     ctx.temp = 0.6f;
     ctx.n_ctx = 2024;
     ctx.n_batch = 16;
-    ctx.n_predict = 18;
+    ctx.n_predict = LLMODEL_TEST_RESPONSE_LEN;
     ctx.repeat_penalty = 1.10f;
     ctx.repeat_last_n = 64;
 
     llmodel_prompt(model, "Did you know?", prompt_cb, response_cb, recalc_cb, &ctx);
 
     EXPECT_FALSE(response.empty()) << "Model didn't generate a response";
-    EXPECT_EQ(response, " The average American eats 20 teaspoons of added sugar every day.") << "Model didn't generate the expected response";
+    if (response.size() >= LLMODEL_TEST_RESPONSE_LEN) response.resize(LLMODEL_TEST_RESPONSE_LEN);
+    EXPECT_EQ(response, LLMODEL_TEST_RESPONSE) << "Model didn't generate the expected response";
 }
 
-TEST(CAPITest, StateSave) {
+/*TEST(GET_SUITE_NAME(CAPITest_), StateSave) {
     savedState = new uint8_t[llmodel_get_state_size(model)];
     EXPECT_GT(llmodel_save_state_data(model, savedState), 0);
 }
 
-TEST(CAPITest, StateRestore) {
+TEST(GET_SUITE_NAME(CAPITest_), StateRestore) {
     EXPECT_GT(llmodel_restore_state_data(model, savedState), 0);
     delete []savedState;
-}
+}*/
 
-TEST(CAPITest, ModelUnload) {
+TEST(GET_SUITE_NAME(CAPITest_), ModelUnload) {
     llmodel_model_destroy(model);
 }
 }
