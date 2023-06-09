5667182ac9f64d218fccbb4b4ada61b1aa72de22
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2023-06-09 11:16:36 -0500

    add comments for index.h

--- gpt4all-bindings/typescript/index.h
+++ gpt4all-bindings/typescript/index.h
@@ -12,28 +12,34 @@ class NodeModelWrapper: public Napi::ObjectWrap<NodeModelWrapper> {
 public:
   NodeModelWrapper(const Napi::CallbackInfo &);
   //~NodeModelWrapper();
-  void Ping(const Napi::CallbackInfo &);
   Napi::Value getType(const Napi::CallbackInfo& info);
   Napi::Value IsModelLoaded(const Napi::CallbackInfo& info);
   Napi::Value StateSize(const Napi::CallbackInfo& info);
+  /**
+   * Prompting the model. This entails spawning a new thread and adding the response tokens
+   * into a thread local string variable.
+   */
   Napi::Value Prompt(const Napi::CallbackInfo& info);
   void SetThreadCount(const Napi::CallbackInfo& info);
   Napi::Value getName(const Napi::CallbackInfo& info);
   Napi::Value ThreadCount(const Napi::CallbackInfo& info);
+  /*
+   * The path that is used to search for the dynamic libraries
+   */
   Napi::Value GetLibraryPath(const Napi::CallbackInfo& info);
+  /**
+   * Creates the LLModel class
+   */
   static Napi::Function GetClass(Napi::Env);
   llmodel_model GetInference();
 private:
+  /**
+   * The underlying inference that interfaces with the C interface
+   */
   std::atomic<std::shared_ptr<llmodel_model>> inference_;
-  // Had to use this instead of the c library in order 
-  // set the type of the model loaded.
-  // causes side effect: type is mutated;
-  //llmodel_model create_model_set_type(const char* c_weights_path);
+
   std::string type;
+  // corresponds to LLModel::name() in typescript
   std::string name;
   static Napi::FunctionReference constructor;
-  // static strings seem to not capture. this is greater because it removes the need for 
-  // capturing stdout and rather capture from this static variable
-  static std::string _res;
-
 };
