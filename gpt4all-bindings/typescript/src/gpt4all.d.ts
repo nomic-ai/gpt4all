5e517c04a356c191942595d25233a561f17c5d27
limez <limez@protonmail.com>
2023-06-10 19:55:55 +0200

    apply autoformatter

--- gpt4all-bindings/typescript/src/gpt4all.d.ts
+++ gpt4all-bindings/typescript/src/gpt4all.d.ts
@@ -1,34 +1,37 @@
 /// <reference types="node" />
-declare module 'gpt4all'
+declare module "gpt4all";
 
-export * from './util.d.ts'
+export * from "./util.d.ts";
 
 /** Type of the model */
-type ModelType = 'gptj' | 'llama' | 'mpt' | 'replit';
+type ModelType = "gptj" | "llama" | "mpt" | "replit";
 
 /**
  * Full list of models available
  */
 interface ModelFile {
     /** List of GPT-J Models */
-    gptj:  | "ggml-gpt4all-j-v1.3-groovy.bin"
-           | "ggml-gpt4all-j-v1.2-jazzy.bin"
-           | "ggml-gpt4all-j-v1.1-breezy.bin"
-           | "ggml-gpt4all-j.bin";
+    gptj:
+        | "ggml-gpt4all-j-v1.3-groovy.bin"
+        | "ggml-gpt4all-j-v1.2-jazzy.bin"
+        | "ggml-gpt4all-j-v1.1-breezy.bin"
+        | "ggml-gpt4all-j.bin";
     /** List Llama Models */
-    llama: | "ggml-gpt4all-l13b-snoozy.bin"
-           | "ggml-vicuna-7b-1.1-q4_2.bin"
-           | "ggml-vicuna-13b-1.1-q4_2.bin"
-           | "ggml-wizardLM-7B.q4_2.bin"
-           | "ggml-stable-vicuna-13B.q4_2.bin"
-           | "ggml-nous-gpt4-vicuna-13b.bin"
-           | "ggml-v3-13b-hermes-q5_1.bin";
+    llama:
+        | "ggml-gpt4all-l13b-snoozy.bin"
+        | "ggml-vicuna-7b-1.1-q4_2.bin"
+        | "ggml-vicuna-13b-1.1-q4_2.bin"
+        | "ggml-wizardLM-7B.q4_2.bin"
+        | "ggml-stable-vicuna-13B.q4_2.bin"
+        | "ggml-nous-gpt4-vicuna-13b.bin"
+        | "ggml-v3-13b-hermes-q5_1.bin";
     /** List of MPT Models */
-    mpt:   | "ggml-mpt-7b-base.bin"
-           | "ggml-mpt-7b-chat.bin"
-           | "ggml-mpt-7b-instruct.bin";
+    mpt:
+        | "ggml-mpt-7b-base.bin"
+        | "ggml-mpt-7b-chat.bin"
+        | "ggml-mpt-7b-instruct.bin";
     /** List of Replit Models */
-    replit:| "ggml-replit-code-v1-3b.bin";
+    replit: "ggml-replit-code-v1-3b.bin";
 }
 
 //mirrors py options
@@ -46,7 +49,6 @@ interface LLModelOptions {
  * This is a base class that provides common functionality for different types of language models.
  */
 declare class LLModel {
-
     /**
      * Initialize a new LLModel.
      * @param path Absolute path to the model file.
@@ -56,10 +58,10 @@ declare class LLModel {
     constructor(options: LLModelOptions);
 
     /** either 'gpt', mpt', or 'llama' or undefined */
-    type() : ModelType|undefined;
+    type(): ModelType | undefined;
 
     /** The name of the model. */
-    name(): ModelFile
+    name(): ModelFile;
 
     /**
      * Get the size of the internal state of the model.
@@ -81,13 +83,13 @@ declare class LLModel {
      */
     setThreadCount(newNumber: number): void;
 
-     /**
-      * Prompt the model with a given input and optional parameters.
-      * This is the raw output from std out.
-      * Use the prompt function exported for a value 
-      * @param q The prompt input.
-      * @param params Optional parameters for the prompt context.
-      * @returns The result of the model prompt.
+    /**
+     * Prompt the model with a given input and optional parameters.
+     * This is the raw output from std out.
+     * Use the prompt function exported for a value
+     * @param q The prompt input.
+     * @param params Optional parameters for the prompt context.
+     * @returns The result of the model prompt.
      */
     raw_prompt(q: string, params?: Partial<LLModelPromptContext>): unknown; // TODO work on return type
 
@@ -95,7 +97,7 @@ declare class LLModel {
      * Whether the model is loaded or not.
      */
     isModelLoaded(): boolean;
-    
+
     /**
      * Where to search for the pluggable backend libraries
      */
@@ -114,7 +116,7 @@ declare class LLModel {
  * @returns {CompletionReturn} The completion result.
  * @example
  * const llmodel = new LLModel(model)
- * const messages = [ 
+ * const messages = [
  * { role: 'system', message: 'You are a weather forecaster.' },
  * { role: 'user', message: 'should i go out today?' } ]
  * const completion = await createCompletion(llmodel, messages, {
@@ -123,7 +125,7 @@ declare class LLModel {
  * })
  * console.log(completion.choices[0].message.content)
  * // No, it's going to be cold and rainy.
-  */
+ */
 declare function createCompletion(
     llmodel: LLModel,
     messages: PromptMessage[],
@@ -139,13 +141,13 @@ interface CompletionOptions extends Partial<LLModelPromptContext> {
      * @default true
      */
     verbose?: boolean;
-    
+
     /**
      * Indicates if the default header is included in the prompt.
      * @default true
      */
     hasDefaultHeader?: boolean;
-    
+
     /**
      * Indicates if the default footer is included in the prompt.
      * @default true
@@ -159,7 +161,7 @@ interface CompletionOptions extends Partial<LLModelPromptContext> {
 interface PromptMessage {
     /** The role of the message. */
     role: "system" | "assistant" | "user";
-    
+
     /** The message content. */
     content: string;
 }
@@ -171,20 +173,20 @@ interface CompletionReturn {
     /** The model name.
      * @type {ModelFile}
      */
-    model : ModelFile[ModelType];
+    model: ModelFile[ModelType];
 
     /** Token usage report. */
     usage: {
-      /** The number of tokens used in the prompt. */
-      prompt_tokens: number;
-      
-      /** The number of tokens used in the completion. */
-      completion_tokens: number;
-      
-      /** The total number of tokens used. */
-      total_tokens: number;
+        /** The number of tokens used in the prompt. */
+        prompt_tokens: number;
+
+        /** The number of tokens used in the completion. */
+        completion_tokens: number;
+
+        /** The total number of tokens used. */
+        total_tokens: number;
     };
-    
+
     /** The generated completions. */
     choices: CompletionChoice[];
 }
@@ -203,53 +205,53 @@ interface CompletionChoice {
 interface LLModelPromptContext {
     /** The size of the raw logits vector. */
     logits_size: number;
-    
+
     /** The size of the raw tokens vector. */
     tokens_size: number;
-    
+
     /** The number of tokens in the past conversation. */
     n_past: number;
-    
+
     /** The number of tokens possible in the context window.
      * @default 1024
      */
     n_ctx: number;
-    
+
     /** The number of tokens to predict.
      * @default 128
      * */
     n_predict: number;
-    
+
     /** The top-k logits to sample from.
      * @default 40
      * */
     top_k: number;
-    
+
     /** The nucleus sampling probability threshold.
      * @default 0.9
      * */
     top_p: number;
-    
+
     /** The temperature to adjust the model's output distribution.
      * @default 0.72
      * */
     temp: number;
-    
+
     /** The number of predictions to generate in parallel.
      * @default 8
      * */
     n_batch: number;
-    
+
     /** The penalty factor for repeated tokens.
      * @default 1
      * */
     repeat_penalty: number;
-    
+
     /** The number of last tokens to penalize.
      * @default 10
      * */
     repeat_last_n: number;
-    
+
     /** The percentage of context to erase if the context window is exceeded.
      * @default 0.5
      * */
@@ -257,27 +259,27 @@ interface LLModelPromptContext {
 }
 
 /**
-  * TODO: Help wanted to implement this
-  */
+ * TODO: Help wanted to implement this
+ */
 declare function createTokenStream(
     llmodel: LLModel,
     messages: PromptMessage[],
     options: CompletionOptions
 ): (ll: LLModel) => AsyncGenerator<string>;
 /**
-  * From python api: 
-  * models will be stored in (homedir)/.cache/gpt4all/`
-  */
+ * From python api:
+ * models will be stored in (homedir)/.cache/gpt4all/`
+ */
 declare const DEFAULT_DIRECTORY: string;
 /**
-  * From python api: 
-  * The default path for dynamic libraries to be stored.
-  * You may separate paths by a semicolon to search in multiple areas.
-  * This searches DEFAULT_DIRECTORY/libraries, cwd/libraries, and finally cwd.
-  */
+ * From python api:
+ * The default path for dynamic libraries to be stored.
+ * You may separate paths by a semicolon to search in multiple areas.
+ * This searches DEFAULT_DIRECTORY/libraries, cwd/libraries, and finally cwd.
+ */
 declare const DEFAULT_LIBRARIES_DIRECTORY: string;
-interface PromptMessage { 
-    role: "system" |"assistant" | "user";
+interface PromptMessage {
+    role: "system" | "assistant" | "user";
     content: string;
 }
 export {
@@ -290,8 +292,5 @@ export {
     createCompletion,
     createTokenStream,
     DEFAULT_DIRECTORY,
-    DEFAULT_LIBRARIES_DIRECTORY
+    DEFAULT_LIBRARIES_DIRECTORY,
 };
-
-
-
--- gpt4all-bindings/typescript/src/gpt4all.js
+++ gpt4all-bindings/typescript/src/gpt4all.js
@@ -1,37 +1,43 @@
-'use strict';
+"use strict";
 
 /// This file implements the gpt4all.d.ts file endings.
 /// Written in commonjs to support both ESM and CJS projects.
-const path = require('node:path');
-const os = require('node:os');
-const { LLModel } = require('node-gyp-build')(path.resolve(__dirname, '..'));
-const util = require('./util.js');
-const DEFAULT_DIRECTORY = path.join(os.homedir(), 'cache', 'gpt4all');
+const path = require("node:path");
+const os = require("node:os");
+const { LLModel } = require("node-gyp-build")(path.resolve(__dirname, ".."));
+const util = require("./util.js");
+const DEFAULT_DIRECTORY = path.join(os.homedir(), "cache", "gpt4all");
 
 exports.download = util.download;
 exports.listModels = util.listModels;
 
 exports.LLModel = LLModel;
 exports.DEFAULT_DIRECTORY = DEFAULT_DIRECTORY;
-exports.DEFAULT_LIBRARIES_DIRECTORY = `${path.join(DEFAULT_DIRECTORY, 'libraries')};${path.resolve('./libraries')};${process.cwd()}`
 
-function createPrompt (messages, hasDefaultHeader, hasDefaultFooter) {
+const librarySearchPaths = [
+    path.join(DEFAULT_DIRECTORY, "libraries"),
+    path.resolve("./libraries"),
+    process.cwd(),
+];
+exports.DEFAULT_LIBRARIES_DIRECTORY = librarySearchPaths.join(";");
+
+function createPrompt(messages, hasDefaultHeader, hasDefaultFooter) {
     let fullPrompt = "";
 
-    for(const message of messages) {
-        if(message.role === 'system') {
+    for (const message of messages) {
+        if (message.role === "system") {
             const systemMessage = message.content + "\n";
-            fullPrompt+= systemMessage;
+            fullPrompt += systemMessage;
         }
     }
-    if(hasDefaultHeader) {
-        fullPrompt+= `### Instruction: 
+    if (hasDefaultHeader) {
+        fullPrompt += `### Instruction: 
         The prompt below is a question to answer, a task to complete, or a conversation 
         to respond to; decide which and write an appropriate response.
         \n### Prompt: 
         `;
     }
-    for(const message of messages) {
+    for (const message of messages) {
         if (message.role === "user") {
             const user_message = "\n" + message["content"];
             fullPrompt += user_message;
@@ -41,55 +47,52 @@ function createPrompt (messages, hasDefaultHeader, hasDefaultFooter) {
             fullPrompt += assistant_message;
         }
     }
-    if(hasDefaultFooter) {
+    if (hasDefaultFooter) {
         fullPrompt += "\n### Response:";
     }
 
     return fullPrompt;
 }
 
-
-
-
 exports.createCompletion = async function (
     llmodel,
     messages,
     options = {
-       hasDefaultHeader: true,
-       hasDefaultFooter: false,
-       verbose : true
-    },
+        hasDefaultHeader: true,
+        hasDefaultFooter: false,
+        verbose: true,
+    }
 ) {
     //creating the keys to insert into promptMaker.
     const fullPrompt = createPrompt(
         messages,
-        options.hasDefaultHeader??true,
+        options.hasDefaultHeader ?? true,
         options.hasDefaultFooter
     );
-    if(options.verbose) {
-        console.log("Sent: "+ fullPrompt);
+    if (options.verbose) {
+        console.log("Sent: " + fullPrompt);
     }
     const promisifiedRawPrompt = new Promise((resolve, rej) => {
         llmodel.raw_prompt(fullPrompt, options, (s) => {
             resolve(s);
-        })
-    });
-    return promisifiedRawPrompt
-        .then( response  => {
-            return {
-                llmodel : llmodel.name(),
-                usage : {
-                    prompt_tokens: fullPrompt.length,
-                    completion_tokens: response.length, //TODO
-                    total_tokens: fullPrompt.length + response.length //TODO
-                },
-                choices: [
-                    {
-                      "message": {
-                        "role": "assistant",
-                        "content": response
-                        }
-                    }
-                ]}
         });
-}
+    });
+    return promisifiedRawPrompt.then((response) => {
+        return {
+            llmodel: llmodel.name(),
+            usage: {
+                prompt_tokens: fullPrompt.length,
+                completion_tokens: response.length, //TODO
+                total_tokens: fullPrompt.length + response.length, //TODO
+            },
+            choices: [
+                {
+                    message: {
+                        role: "assistant",
+                        content: response,
+                    },
+                },
+            ],
+        };
+    });
+};
