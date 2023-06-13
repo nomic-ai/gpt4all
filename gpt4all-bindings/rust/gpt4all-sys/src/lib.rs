d06d0222d55f07cb11d28a1fdaa47fe18461684c
marcusdunn <marcus.s.dunn@gmail.com>
2023-06-12 18:10:57 -0700

    removed test referencing my file system

--- gpt4all-bindings/rust/gpt4all-sys/src/lib.rs
+++ gpt4all-bindings/rust/gpt4all-sys/src/lib.rs
@@ -15,11 +15,4 @@ mod tests {
         let ptr = unsafe { llmodel_model_create(invalid_model_path.as_ptr()) };
         assert!(ptr.is_null());
     }
-
-    #[test]
-    fn test_calling_create() {
-        let model_path = CString::new("/home/marcusdunn/IdeaProjects/gpt4all/gpt4all-bindings/rust/gpt4all/ggml-gpt4all-j-v1.3-groovy.bin").unwrap();
-        let ptr = unsafe { llmodel_model_create(model_path.as_ptr()) };
-        assert!(!ptr.is_null());
-    }
 }
