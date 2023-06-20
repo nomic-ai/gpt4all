4b7b9975c57a179670e5c1c22aeb31220aaaa97e
mvenditto <venditto.matteo@gmail.com>
2023-06-20 21:25:11 +0200

    add lib loading tests + remove dummy test

---
+++ gpt4all-bindings/csharp/Gpt4All.Tests/PlatformSpecificFactAttribute.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace Gpt4All.Tests;
+
+public static class Platforms
+{
+    public const string Windows = "windows";
+    public const string Linux = "linux";
+    public const string MacOS = "macOS";
+}
+
+/// <summary>
+/// This attribute ensures the Fact is only run on the specified platform.
+/// </summary>
+/// <remarks>
+/// <see cref="OperatingSystem.IsOSPlatform(string)"/> for info about the platform string.
+/// </remarks>
+public class PlatformSpecificFactAttribute : FactAttribute
+{
+    public PlatformSpecificFactAttribute(string platform)
+    {
+        if (!OperatingSystem.IsOSPlatform(platform))
+        {
+            Skip = $"Test only runs on {platform}.";
+        }
+    }
+}
