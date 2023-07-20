a81dd1a6dd666752dda2a3acb529377d61bfbd76
Zach Nussbaum <zanussbaum@gmail.com>
2023-07-20 20:38:44 +0000

    tests: fix tests

--- gpt4all-api/gpt4all_api/app/tests/test_endpoints.py
+++ gpt4all-api/gpt4all_api/app/tests/test_endpoints.py
@@ -9,16 +9,15 @@ openai.api_key = "not needed for a local LLM"
 
 
 def test_completion():
-    model = "ggml-mpt-7b-chat"
+    model = "ggml-mpt-7b-chat.bin"
     prompt = "Who is Michael Jordan?"
     response = openai.Completion.create(
         model=model, prompt=prompt, max_tokens=50, temperature=0.28, top_p=0.95, n=1, echo=True, stream=False
     )
     assert len(response['choices'][0]['text']) > len(prompt)
-    print(response)
 
 def test_streaming_completion():
-    model = "gpt4all-j-v1.3-groovy"
+    model = "ggml-mpt-7b-chat.bin"
     prompt = "Who is Michael Jordan?"
     tokens = []
     for resp in openai.Completion.create(
@@ -37,11 +36,10 @@ def test_streaming_completion():
 
 
 def test_batched_completion():
-    model = "ggml-mpt-7b-chat"
+    model = "ggml-mpt-7b-chat.bin"
     prompt = "Who is Michael Jordan?"
     response = openai.Completion.create(
         model=model, prompt=[prompt] * 3, max_tokens=50, temperature=0.28, top_p=0.95, n=1, echo=True, stream=False
     )
     assert len(response['choices'][0]['text']) > len(prompt)
     assert len(response['choices']) == 3
-    print(response)
