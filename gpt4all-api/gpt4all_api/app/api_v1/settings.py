8aba2c9009fb6bc723f623c614e265b41722e4e3
Zach Nussbaum <zanussbaum@gmail.com>
2023-07-21 14:13:29 -0500

    GPU Inference Server (#1112)
    * feat: local inference server
    
    * fix: source to use bash + vars
    
    * chore: isort and black
    
    * fix: make file + inference mode
    
    * chore: logging
    
    * refactor: remove old links
    
    * fix: add new env vars
    
    * feat: hf inference server
    
    * refactor: remove old links
    
    * test: batch and single response
    
    * chore: black + isort
    
    * separate gpu and cpu dockerfiles
    
    * moved gpu to separate dockerfile
    
    * Fixed test endpoints
    
    * Edits to API. server won't start due to failed instantiation error
    
    * Method signature
    
    * fix: gpu_infer
    
    * tests: fix tests
    
    ---------
    
    Co-authored-by: Andriy Mulyar <andriy.mulyar@gmail.com>

--- gpt4all-api/gpt4all_api/app/api_v1/settings.py
+++ gpt4all-api/gpt4all_api/app/api_v1/settings.py
@@ -5,6 +5,14 @@ class Settings(BaseSettings):
     app_environment = 'dev'
     model: str = 'ggml-mpt-7b-chat.bin'
     gpt4all_path: str = '/models'
+    inference_mode: str = "cpu"
+    hf_inference_server_host: str = "http://gpt4all_gpu:80/generate"
+
+    temp: float = 0.18
+    top_p: float = 1.0
+    top_k: int = 50
+    repeat_penalty: float = 1.18
+
 
 
 settings = Settings()
--- gpt4all-api/gpt4all_api/app/tests/test_endpoints.py
+++ gpt4all-api/gpt4all_api/app/tests/test_endpoints.py
@@ -2,30 +2,22 @@
 Use the OpenAI python API to test gpt4all models.
 """
 import openai
+
 openai.api_base = "http://localhost:4891/v1"
 
 openai.api_key = "not needed for a local LLM"
 
 
 def test_completion():
-    model = "gpt4all-j-v1.3-groovy"
+    model = "ggml-mpt-7b-chat.bin"
     prompt = "Who is Michael Jordan?"
     response = openai.Completion.create(
-        model=model,
-        prompt=prompt,
-        max_tokens=50,
-        temperature=0.28,
-        top_p=0.95,
-        n=1,
-        echo=True,
-        stream=False
+        model=model, prompt=prompt, max_tokens=50, temperature=0.28, top_p=0.95, n=1, echo=True, stream=False
     )
     assert len(response['choices'][0]['text']) > len(prompt)
-    print(response)
-
 
 def test_streaming_completion():
-    model = "gpt4all-j-v1.3-groovy"
+    model = "ggml-mpt-7b-chat.bin"
     prompt = "Who is Michael Jordan?"
     tokens = []
     for resp in openai.Completion.create(
@@ -42,10 +34,12 @@ def test_streaming_completion():
     assert (len(tokens) > 0)
     assert (len("".join(tokens)) > len(prompt))
 
-# def test_chat_completions():
-#     model = "gpt4all-j-v1.3-groovy"
-#     prompt = "Who is Michael Jordan?"
-#     response = openai.ChatCompletion.create(
-#         model=model,
-#         messages=[]
-#     )
+
+def test_batched_completion():
+    model = "ggml-mpt-7b-chat.bin"
+    prompt = "Who is Michael Jordan?"
+    response = openai.Completion.create(
+        model=model, prompt=[prompt] * 3, max_tokens=50, temperature=0.28, top_p=0.95, n=1, echo=True, stream=False
+    )
+    assert len(response['choices'][0]['text']) > len(prompt)
+    assert len(response['choices']) == 3
