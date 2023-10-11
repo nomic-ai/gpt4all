10dd89fe98413dcb4be05588f3973b77244690ed
Aaron Miller <apage43@ninjawhale.com>
2023-10-11 14:35:10 -0700

    fix embed4all filename
    https://discordapp.com/channels/1076964370942267462/1093558720690143283/1161778216462192692
    
    Signed-off-by: Aaron Miller <apage43@ninjawhale.com>

--- gpt4all-bindings/python/gpt4all/gpt4all.py
+++ gpt4all-bindings/python/gpt4all/gpt4all.py
@@ -39,7 +39,7 @@ class Embed4All:
         Args:
             n_threads: number of CPU threads used by GPT4All. Default is None, then the number of threads are determined automatically.
         """
-        self.gpt4all = GPT4All(model_name or 'ggml-all-MiniLM-L6-v2-f16.gguf', n_threads=n_threads, **kwargs)
+        self.gpt4all = GPT4All(model_name or 'all-MiniLM-L6-v2-f16.gguf', n_threads=n_threads, **kwargs)
 
     def embed(self, text: str) -> List[float]:
         """
