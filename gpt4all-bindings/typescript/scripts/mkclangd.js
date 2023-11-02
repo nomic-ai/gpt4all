89927f0b38ac12c6361b7c8c526954e32d3523a5
jacob <jacoobes@sern.dev>
2023-11-02 10:55:24 -0500

    more

--- gpt4all-bindings/typescript/scripts/mkclangd.js
+++ gpt4all-bindings/typescript/scripts/mkclangd.js
@@ -1,21 +1,21 @@
 /// makes compile_flags.txt for clangd server support with this project
 /// run this with typescript as your cwd
+//
+//for debian users make sure to install libstdc++-12-dev
 
 const nodeaddonapi=require('node-addon-api').include;
 
+const fsp = require('fs/promises');
+const { existsSync, readFileSync } = require('fs');
+const assert = require('node:assert');
 const findnodeapih = () => {
-    const platform = process.platform;
-    switch(platform) {
-        case 'win32': {
-            
-        };
-        case 'linux': {
-
-        };
-        case 'darwin': {
-
-        };
-    }
+    assert(existsSync("./build"), "Haven't built the application once yet. run node scripts/prebuild.js");
+    const dir = readFileSync("./build/config.gypi", 'utf8');
+    const nodedir_line = dir.match(/"nodedir": "([^"]+)"/);
+    assert(nodedir_line, "Found no matches")
+    assert(nodedir_line[1]);
+    console.log("node_api.h found at: ", nodedir_line[1]);
+    return nodedir_line[1]+"/include/node";
 };
 
 const knownIncludes = [
@@ -24,7 +24,9 @@ const knownIncludes = [
     '-I',
     nodeaddonapi.substring(1, nodeaddonapi.length-1),
     '-I',
-    '../../gpt4all-backend'
+    '../../gpt4all-backend',
+    '-I',
+    findnodeapih()
 ];
 const knownFlags = [
     "-x",
@@ -32,7 +34,6 @@ const knownFlags = [
     '-std=c++17'
 ];
 
-const fsp = require('fs/promises');
 
 const output = knownFlags.join('\n')+'\n'+knownIncludes.join('\n');
 
