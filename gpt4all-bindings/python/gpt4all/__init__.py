1ad916d1f9500a71f061099057d0ae32428e6ca2
Jaehyun Ahn <jaehyunahn@sogang.ac.kr>
2023-12-05 11:03:19 +0900

    Update __init__.py
    Following PEP440, adding __version__ variable into initialized package. https://peps.python.org/pep-0440/
    
    Signed-off-by: Jaehyun Ahn <jaehyunahn@sogang.ac.kr>

--- gpt4all-bindings/python/gpt4all/__init__.py
+++ gpt4all-bindings/python/gpt4all/__init__.py
@@ -1,2 +1,20 @@
 from .gpt4all import Embed4All, GPT4All  # noqa
 from .pyllmodel import LLModel  # noqa
+
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+#
+# Generic release markers:
+#   X.Y.0   # For first release after an increment in Y
+#   X.Y.Z   # For bugfix releases
+#
+# Admissible pre-release markers:
+#   X.Y.ZaN   # Alpha release
+#   X.Y.ZbN   # Beta release
+#   X.Y.ZrcN  # Release Candidate
+#   X.Y.Z     # Final release
+#
+# Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
+# 'X.Y.dev0' is the canonical version of 'X.Y.dev'
+#
+__version__ = "2.0.2"
