eef604fd642384f25aff48df187c9ac94a267495
Jared Van Bortel <jared@nomic.ai>
2024-01-12 09:38:16 -0500

    python: release bindings version 2.1.0
    The backend has a breaking change for Falcon and MPT models, so we need
    to make a new release.

--- gpt4all-bindings/python/setup.py
+++ gpt4all-bindings/python/setup.py
@@ -61,7 +61,7 @@ copy_prebuilt_C_lib(SRC_CLIB_DIRECtORY,
 
 setup(
     name=package_name,
-    version="2.0.2",
+    version="2.1.0",
     description="Python bindings for GPT4All",
     author="Nomic and the Open Source Community",
     author_email="support@nomic.ai",
