fb649db11d8d2e832b2f0bc0c38dac11993cffa1
limez <limez@protonmail.com>
2024-03-17 17:33:34 +0100

    fix systemPrompt usage
    Signed-off-by: limez <limez@protonmail.com>

--- gpt4all-bindings/typescript/spec/llmodel.mjs
+++ gpt4all-bindings/typescript/spec/llmodel.mjs
@@ -2,7 +2,9 @@ import { LLModel, createCompletion, DEFAULT_DIRECTORY, DEFAULT_LIBRARIES_DIRECTO
 
 const model = await loadModel(
     'mistral-7b-openorca.gguf2.Q4_0.gguf',
-    { verbose: true, device: 'gpu' }
+    { verbose: true, 
+        // device: 'gpu' 
+    }
 );
 const ll = model.llm;
 
@@ -29,15 +31,24 @@ console.log("Default directory for libraries", DEFAULT_LIBRARIES_DIRECTORY);
 console.log("Has GPU", ll.hasGpuDevice());
 console.log("gpu devices", ll.listGpu())
 console.log("Required Mem in bytes", ll.memoryNeeded())
+
+// to ingest a custom system prompt without using a chat session.
+await createCompletion(model, '<|im_start|>system\nYou are an advanced mathematician.\n<|im_end|>\n', {
+    promptTemplate: "%1",
+    nPredict: 0,
+    special: true,
+})
 const completion1 = await createCompletion(model, 'What is 1 + 1?', {
     verbose: true,
 })
-console.log(completion1.message)
+console.log(`🤖 > ${completion1.message}`)
 //Very specific: 
 // tested on Ubuntu 22.0, Linux Mint, if I set nPast to 100, the app hangs.
 const completion2 = await createCompletion(model, 'And if we add two?', {  verbose: true  })
+console.log(`🤖 > ${completion2.message}`)
 
-console.log(completion2.message)
 
 //CALLING DISPOSE WILL INVALID THE NATIVE MODEL. USE THIS TO CLEANUP
 model.dispose()
+
+console.log('model disposed, exiting...')
