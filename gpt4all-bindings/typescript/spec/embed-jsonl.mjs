a8540c8ea9dde986cb1a8d21386d3b46fbeb4cfa
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2024-03-20 00:38:13 -0500

    embed jsonl dataset from zip example
    Signed-off-by: Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>

--- gpt4all-bindings/typescript/spec/embed-jsonl.mjs
+++ gpt4all-bindings/typescript/spec/embed-jsonl.mjs
@@ -2,20 +2,9 @@ import { loadModel, createEmbedding } from '../src/gpt4all.js'
 import { createGunzip, createGzip, createUnzip } from 'node:zlib';
 import { Readable } from 'stream'
 import readline  from 'readline'
-const embedder = await loadModel("nomic-embed-text-v1.5.f16.gguf", { verbose: true, type: 'embedding' })
+const embedder = await loadModel("nomic-embed-text-v1.5.f16.gguf", { verbose: true, type: 'embedding', gpu: true })
 console.log("Running with", embedder.llm.threadCount(), "threads");
 
-function cosineDistance(a, b) {
-    let dotProduct = 0;
-    let magA = 0;
-    let magB = 0;
-    for (let i = 0; i < a.length; i++) {
-        dotProduct += a[i] * b[i];
-        magA += a[i] * a[i];
-        magB += b[i] * b[i];
-    }
-    return 1 - (dotProduct / (Math.sqrt(magA) * Math.sqrt(magB)));
-}
 
 const unzip = createGunzip();
 const url = "https://huggingface.co/datasets/sentence-transformers/embedding-training-data/resolve/main/squad_pairs.jsonl.gz"
@@ -30,9 +19,7 @@ const lineReader = readline.createInterface({
 lineReader.on('line', line => {
     //pairs of questions and answers
     const question_answer = JSON.parse(line)
-    console.log("embedding", question_answer)
     console.log(createEmbedding(embedder, question_answer))
-
 })
 
 
