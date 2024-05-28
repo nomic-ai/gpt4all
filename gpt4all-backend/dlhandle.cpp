f65a0b16cc0c5411842fc9da241510ac71ec99f5
Jared Van Bortel <jared@nomic.ai>
2024-05-28 16:21:20 -0400

    dlhandle: suppress DLL errors on Windows
    Sometimes the CUDA runtime cannot be loaded. Do not open an error dialog
    in this case, as CUDA is an optional feature of GPT4All.
    
    Signed-off-by: Jared Van Bortel <jared@nomic.ai>

--- gpt4all-backend/dlhandle.cpp
+++ gpt4all-backend/dlhandle.cpp
@@ -3,6 +3,7 @@
 #ifndef _WIN32
 #   include <dlfcn.h>
 #else
+#   include <cassert>
 #   include <sstream>
 #   define WIN32_LEAN_AND_MEAN
 #   ifndef NOMINMAX
@@ -34,11 +35,20 @@ void *Dlhandle::get_internal(const char *symbol) const {
 #else // defined(_WIN32)
 
 Dlhandle::Dlhandle(const fs::path &fpath) {
-    auto afpath = fs::absolute(fpath);
+    fs::path afpath = fs::absolute(fpath);
+
+    // Suppress the "Entry Point Not Found" dialog, caused by outdated nvcuda.dll from the GPU driver
+    UINT lastErrorMode = GetErrorMode();
+    UINT success = SetErrorMode(lastErrorMode | SEM_FAILCRITICALERRORS);
+    assert(success);
+
     chandle = LoadLibraryExW(afpath.c_str(), NULL, LOAD_LIBRARY_SEARCH_DEFAULT_DIRS | LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR);
 
+    success = SetErrorMode(lastErrorMode);
+    assert(success);
+
     if (!chandle) {
-        auto err = GetLastError();
+        DWORD err = GetLastError();
         std::ostringstream ss;
         ss << "LoadLibraryExW(\"" << fpath.filename().string() << "\") failed with error 0x" << std::hex << err;
         throw Exception(ss.str());
