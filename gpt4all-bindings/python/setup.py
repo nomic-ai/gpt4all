297e1171a71db7030d03a4b0670a7b0cd3de21f5
Jared Van Bortel <jared@nomic.ai>
2024-08-04 17:01:18 -0400

    python: bump version to 2.8.0 (3.0.0 was never released)
    Signed-off-by: Jared Van Bortel <jared@nomic.ai>

--- gpt4all-bindings/python/setup.py
+++ gpt4all-bindings/python/setup.py
@@ -68,7 +68,7 @@ def get_long_description():
 
 setup(
     name=package_name,
-    version="3.0.0",
+    version="2.8.0",
     description="Python bindings for GPT4All",
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
