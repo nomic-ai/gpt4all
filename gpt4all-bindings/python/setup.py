1f2294ed7373e7ab82b6a8b2317e4400c83f1d26
Jared Van Bortel <jared@nomic.ai>
2024-08-05 13:36:18 -0400

    python: prepare to release v2.8.0 (#2794)
    Signed-off-by: Jared Van Bortel <jared@nomic.ai>

--- gpt4all-bindings/python/setup.py
+++ gpt4all-bindings/python/setup.py
@@ -68,7 +68,7 @@ def get_long_description():
 
 setup(
     name=package_name,
-    version="2.8.0.dev0",
+    version="2.8.0",
     description="Python bindings for GPT4All",
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
