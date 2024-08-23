c9dda3df0d86c78c582beda14174fab93c99b62b
3Simplex <10260755+3Simplex@users.noreply.github.com>
2024-08-23 12:36:54 -0400

    Update button for offline installer now points to releases. (#2888)
    Signed-off-by: 3Simplex <10260755+3Simplex@users.noreply.github.com>

--- gpt4all-chat/llm.cpp
+++ gpt4all-chat/llm.cpp
@@ -51,7 +51,7 @@ bool LLM::checkForUpdates() const
 {
 #ifdef GPT4ALL_OFFLINE_INSTALLER
 #   pragma message(__FILE__ ": WARNING: offline installer build will not check for updates!")
-    return QDesktopServices::openUrl(QUrl("https://gpt4all.io/"));
+    return QDesktopServices::openUrl(QUrl("https://github.com/nomic-ai/gpt4all/releases"));
 #else
     Network::globalInstance()->trackEvent("check_for_updates");
 
