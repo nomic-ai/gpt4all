a5667b4727090001d5c0ddca5f5aa3a138dffd1f
Jared Van Bortel <jared@nomic.ai>
2024-10-08 12:27:56 -0400

    python: clean up CUDA locating code
    With our new use of _operator_call as a decorator, we can avoid exposing
    _load_cuda, rtver, and blasver at module scope.
    
    Signed-off-by: Jared Van Bortel <jared@nomic.ai>

--- gpt4all-bindings/python/gpt4all/_pyllmodel.py
+++ gpt4all-bindings/python/gpt4all/_pyllmodel.py
@@ -58,33 +58,38 @@ if platform.system() == "Windows":
         """), file=sys.stderr)
 
 
-def _load_cuda(rtver: str, blasver: str) -> None:
-    if platform.system() == "Linux":
-        cudalib   = f"lib/libcudart.so.{rtver}"
-        cublaslib = f"lib/libcublas.so.{blasver}"
-    else:  # Windows
-        cudalib   = fr"bin\cudart64_{rtver.replace('.', '')}.dll"
-        cublaslib = fr"bin\cublas64_{blasver}.dll"
+cuda_found: bool = False
 
-    # preload the CUDA libs so the backend can find them
-    ctypes.CDLL(os.path.join(cuda_runtime.__path__[0], cudalib), mode=ctypes.RTLD_GLOBAL)
-    ctypes.CDLL(os.path.join(cublas.__path__[0], cublaslib), mode=ctypes.RTLD_GLOBAL)
+@_operator_call
+def find_cuda() -> None:
+    global cuda_found
+
+    def _load_cuda(rtver: str, blasver: str) -> None:
+        if platform.system() == "Linux":
+            cudalib   = f"lib/libcudart.so.{rtver}"
+            cublaslib = f"lib/libcublas.so.{blasver}"
+        else:  # Windows
+            cudalib   = fr"bin\cudart64_{rtver.replace('.', '')}.dll"
+            cublaslib = fr"bin\cublas64_{blasver}.dll"
+
+        # preload the CUDA libs so the backend can find them
+        ctypes.CDLL(os.path.join(cuda_runtime.__path__[0], cudalib), mode=ctypes.RTLD_GLOBAL)
+        ctypes.CDLL(os.path.join(cublas.__path__[0], cublaslib), mode=ctypes.RTLD_GLOBAL)
 
 
-# Find CUDA libraries from the official packages
-cuda_found = False
-if platform.system() in ("Linux", "Windows"):
-    try:
-        from nvidia import cuda_runtime, cublas
-    except ImportError:
-        pass  # CUDA is optional
-    else:
-        for rtver, blasver in [("12", "12"), ("11.0", "11")]:
-            try:
-                _load_cuda(rtver, blasver)
-                cuda_found = True
-            except OSError:  # dlopen() does not give specific error codes
-                pass  # try the next one
+    # Find CUDA libraries from the official packages
+    if platform.system() in ("Linux", "Windows"):
+        try:
+            from nvidia import cuda_runtime, cublas
+        except ImportError:
+            pass  # CUDA is optional
+        else:
+            for rtver, blasver in [("12", "12"), ("11.0", "11")]:
+                try:
+                    _load_cuda(rtver, blasver)
+                    cuda_found = True
+                except OSError:  # dlopen() does not give specific error codes
+                    pass  # try the next one
 
 
 # TODO: provide a config file to make this more robust
