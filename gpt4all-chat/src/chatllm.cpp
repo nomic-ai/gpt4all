126447bac5ab0c6361d8b88631b7a0932ec16848
Adam Treat <treat.adam@gmail.com>
2025-02-06 11:25:05 -0500

    Changes.
    Signed-off-by: Adam Treat <treat.adam@gmail.com>

--- gpt4all-chat/src/chatllm.cpp
+++ gpt4all-chat/src/chatllm.cpp
@@ -976,7 +976,8 @@ public:
     {
         Q_UNUSED(bufferIdx)
         try {
-            m_cllm->m_chatModel->setResponseValue(response.trimmed());
+            QString r = response;
+            m_cllm->m_chatModel->setResponseValue(removeLeadingWhitespace(r));
         } catch (const std::exception &e) {
             // We have a try/catch here because the main thread might have removed the response from
             // the chatmodel by erasing the conversation during the response... the main thread sets
@@ -991,7 +992,7 @@ public:
     bool onRegularResponse() override
     {
         auto respStr = QString::fromUtf8(m_result->response);
-        return onBufferResponse(removeLeadingWhitespace(respStr), 0);
+        return onBufferResponse(respStr, 0);
     }
 
     bool getStopGenerating() const override
