067d3d4ad0678c94a006cd14d1b6e2d4330d7251
Jean van Kasteel <vankasteelj@gmail.com>
2025-02-26 16:01:55 +0100

    Update gpt4all_fr_FR.ts
    change the language= field
    
    Signed-off-by: Jean van Kasteel <vankasteelj@gmail.com>

--- gpt4all-chat/translations/gpt4all_fr_FR.ts
+++ gpt4all-chat/translations/gpt4all_fr_FR.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_US">
+<TS version="2.1" language="fr_FR">
 <context>
     <name>AddCollectionView</name>
     <message>
